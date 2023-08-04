cc946af0340ab38d52110deb929edb91c8f36743
Julie Hinge <78808112+juliehinge@users.noreply.github.com>
2023-08-04 17:26:42 +0200

    added  option to upload multiple files within each category

--- page_eleven.py
+++ page_eleven.py
@@ -6,98 +6,93 @@ import pandas as pd
 from p import Pages
 import re
 
-
-
-
 class PageEleven(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller 
 
-        self.flag1 = False 
-        self.flag2 = False 
-        self.flag3 = False  # New flag for the additional CSV/TXT file
-        self.alpha_list = []; self.vector_list = []
+        self.flags = {}
+        self.paths = {} 
+        self.file_data = {}
 
         ttk.Label(self, text="Upload your CSV files please", font = ("bold", 20)).grid(row=1, column=0, padx = (10,30), pady = (10,30), columnspan=3)
 
-        Btn_import1 = ttk.Button(self, text='Upload Positions', command=lambda:self.import_csv_data(1));  Btn_import1.grid(row=2, column=0, sticky='w')
-        Btn_import2 = ttk.Button(self, text='Upload Directions', command=lambda:self.import_csv_data(2));  Btn_import2.grid(row=2, column=1, sticky='w')
-        Btn_import3 = ttk.Button(self, text='Upload Energy', command=lambda:self.import_csv_data(3));  Btn_import3.grid(row=2, column=2, sticky='w')  # New button for the additional CSV/TXT file
-        Btn_ok = ttk.Button(self, text='OK', command=lambda:self.open_window());  Btn_ok.grid(row=3, column=0, sticky='w')
-        Btn_back = ttk.Button(self, text="Back", command=lambda: controller.show_frame("PageTen")); Btn_back.grid(row=3, column=1, sticky='w')
+        ttk.Label(self, text="Enter the number of files you want to upload for each category:", font=("bold", 10)).grid(row=2, column=0, padx=10, pady=10)
+        self.num_files = tk.IntVar(value=0)
+        ttk.Entry(self, textvariable=self.num_files).grid(row=2, column=1, padx=10, pady=10)
+        ttk.Button(self, text='Set', command=self.set_num_files).grid(row=2, column=2, sticky='w')
 
-        self.path1 = StringVar(self, value=' ')
-        self.text1 = ttk.Label(self, textvariable = self.path1).grid(row=4, column=0, columnspan=5, pady = 5)
-        self.path2 = StringVar(self, value=' ')
-        self.text2 = ttk.Label(self, textvariable = self.path2).grid(row=5, column=0, columnspan=5, pady = 5)
-        self.path3 = StringVar(self, value=' ')  # New path variable for the additional CSV/TXT file
-        self.text3 = ttk.Label(self, textvariable = self.path3).grid(row=6, column=0, columnspan=5, pady = 5)  # New label for the additional CSV/TXT file
+        self.upload_buttons = []
+        self.ok_button = None
+        self.back_button = None
 
-    def import_csv_data(self, csv_number):
-        csv_file_path = askopenfilename() 
+    def set_num_files(self):
+        for button in self.upload_buttons:
+            button.grid_forget()
+        if self.ok_button:
+            self.ok_button.grid_forget()
+        if self.back_button:
+            self.back_button.grid_forget()
 
-        if csv_number == 1:
-            path = self.path1
-        elif csv_number == 2:
-            path = self.path2
-        elif csv_number == 3:  # New condition for the additional CSV/TXT file
-            path = self.path3
+        categories = ['Positions', 'Directions', 'Energies']
+        for category_index, category in enumerate(categories, start=1):
+            button = ttk.Button(self, text=f'Upload {category} Files', command=lambda category=category: self.import_csv_data(category))
+            button.grid(row=3+category_index, column=0, sticky='w')
+            self.upload_buttons.append(button)
 
-        path.set(csv_file_path)
+            self.paths[category] = StringVar(self, value=' ')
+            ttk.Label(self, textvariable=self.paths[category]).grid(row=3+category_index, column=1, columnspan=5, pady=5)
+            
+            for i in range(self.num_files.get()):
+                file_number = (category, i+1)  # using tuple (category, index) as the key
+                self.flags[file_number] = False 
 
-        if "csv" not in csv_file_path and "txt" not in csv_file_path: 
-            path.set("Please input a CSV or TXT file only")
-            if csv_number == 1:
-                self.flag1 = False 
-            elif csv_number == 2:
-                self.flag2 = False 
-            elif csv_number == 3:  # New condition for the additional CSV/TXT file
-                self.flag3 = False
+        self.ok_button = ttk.Button(self, text='OK', command=self.open_window)
+        self.ok_button.grid(row=5+category_index, column=0, sticky='w')
+        self.back_button = ttk.Button(self, text="Back", command=lambda: self.controller.show_frame("PageTen"))
+        self.back_button.grid(row=5+category_index, column=1, sticky='w')
 
-        else:
-            path.set(csv_file_path)
-            if csv_number == 1:
-                self.flag1 = True 
-            elif csv_number == 2:
-                self.flag2 = True 
-            elif csv_number == 3:  # New condition for the additional CSV/TXT file
-                self.flag3 = True 
+    def import_csv_data(self, category):
+        csv_file_paths = askopenfilename(multiple=True)  # allows multiple file selection
 
-            df = pd.read_csv(csv_file_path) 
+        path_list = []
+        for i, csv_file_path in enumerate(csv_file_paths):
+            file_number = (category, i+1)  # using tuple (category, index) as the key
+            path = self.paths.get(category)
 
-            # Read only the first row from the CSV and get the column names
-            header_row = pd.read_csv(csv_file_path, nrows=1).columns
-            # Check each column name for digits and return False if any are found
-            is_header = all(not bool(re.search(r'\d', header)) for header in header_row)
+            if not path:
+                continue  # Skip if the file number doesn't exist (i.e., more files were selected than specified)
 
-            data = []
-            if is_header:
-                for _, row in df.iterrows():
-                    data.append(row.tolist())
+            path_list.append(csv_file_path[:5])  # Append the first 5 letters of the file path to the list
+            
+            if "csv" not in csv_file_path and "txt" not in csv_file_path: 
+                self.flags[file_number] = False
             else:
-                df = pd.read_csv(csv_file_path, header=None) 
-                for _, row in df.iterrows():
-                    data.append(row.tolist())
+                self.flags[file_number] = True 
 
-            if csv_number == 1:
-                Pages.pos_vector = data
-            elif csv_number == 2:
-                Pages.dir_vector = data
-            elif csv_number == 3:  # New condition for the additional CSV/TXT file
-                Pages.ener_vector = data  # Update this to the variable you want to use for the additional data
+                df = pd.read_csv(csv_file_path) 
+                header_row = pd.read_csv(csv_file_path, nrows=1).columns
+                is_header = all(not bool(re.search(r'\d', header)) for header in header_row)
 
- 
+                data = []
+                if is_header:
+                    for _, row in df.iterrows():
+                        data.append(row.tolist())
+                else:
+                    df = pd.read_csv(csv_file_path, header=None) 
+                    for _, row in df.iterrows():
+                        data.append(row.tolist())
+
+                self.file_data[file_number] = data
+
+        self.paths[category].set(','.join(path_list))  # Set the variable to the joined string
 
     def open_window(self):
-        if self.flag1 and self.flag2: 
+        if all(self.flags.values()): 
+            Pages.file_data = self.file_data
             self.controller.show_frame("PageTwelve")
-
         else:
-            if not self.flag1:
-                self.path1.set("Please upload a CSV or TXT file for the positions of the particles")
-            if not self.flag2:
-                self.path2.set("Please upload a CSV or TXT file for the directions of the particles")
-            if not self.flag3:
-                self.path3.set("Please upload a CSV or TXT file for the energies of the particles")
-
+            for file_number, flag in self.flags.items():
+                if not flag:
+                    category, i = file_number
+                    self.paths[category].set(f"Please upload a CSV or TXT file for {category} file {i}")
